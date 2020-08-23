5f920cd9e651f7029f8ecf0ce3f4802b8064f0ae
ivajimlop <69108639+ivajimlop@users.noreply.github.com>
2020-08-23 09:49:42 +0200

    Add files via upload

---
+++ build/precache-manifest.7363438ad51d860f38279fec39d7b940.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9363ecc937b6404b677f33ad4c40baec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8f28ef0f0dbe661c025",
+    "url": "/static/css/main.480fbee7.chunk.css"
+  },
+  {
+    "revision": "35ff1e67a448da6e77b4",
+    "url": "/static/js/2.ce226546.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ce226546.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8f28ef0f0dbe661c025",
+    "url": "/static/js/main.440d3d8b.chunk.js"
+  },
+  {
+    "revision": "29630ad1ca7e967d236a",
+    "url": "/static/js/runtime-main.ea644902.js"
+  }
+]);
